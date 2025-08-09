48dcc301cd1eee5b0a9d039b99b66772adfbc806
Sachin-Engineer <sachin.engineer.22@gmail.com>
2025-08-09 22:36:27 +0530

    third commit

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
 const checkBoxList = document.querySelectorAll('.custom-checkbox')
 const goalInputList = document.querySelectorAll('.goal-input')
 const progressBar = document.querySelector('.progress-bar')
+const progressValue = document.querySelector('.progress-value')
+const progressValueSpan = document.querySelector('.progress-value span')
 
-let noOfFilledGoalInput;
+let noOfFilledGoalInput = 0;
+let noOfCompletedClass = 0;
 
 checkBoxList.forEach((checkBox) => {
     checkBox.addEventListener('click', () => {
-        noOfFilledGoalInput = 0;
-
-        goalInputList.forEach((goalInput) => {
-            if (goalInput.value) {
-                noOfFilledGoalInput++;
-                console.log(noOfFilledGoalInput)
-            }
-        })
-
         if (noOfFilledGoalInput === 3) {
             // progressBar.classList.remove('show-error')
             checkBox.parentElement.classList.toggle('completed')
+            // if(checkBox.parentElement.classList.contains('completed')) {
+            //     noOfCompletedClass++
+            //     console.log(noOfCompletedClass)
+            //     if(noOfCompletedClass === 1) {
+            //         progressValue.style.width = '33%'
+            //         progressValueSpan.textContent = '1/3 completed'
+            //     } else if(noOfCompletedClass === 2) {
+            //         progressValue.style.width = '66%'
+            //         progressValueSpan.textContent = '2/3 completed'
+            //     } else if(noOfCompletedClass === 3) {
+            //         progressValue.style.width = '100%'
+            //         progressValueSpan.textContent = '3/3 completed'
+            //         noOfCompletedClass = 0;
+            //     } else {
+            //         progressValue.style.width = '0%'
+            //         progressValueSpan.textContent = ''
+            //     }
+            // }
         } else {
             progressBar.classList.add('show-error')
         }
@@ -25,15 +37,15 @@ checkBoxList.forEach((checkBox) => {
 })
 
 goalInputList.forEach((goalInput) => {
-    noOfFilledGoalInput = 0;
-    if (goalInput.value) {
-        noOfFilledGoalInput++;
-        console.log(noOfFilledGoalInput)
-    }
-})
-
-if(noOfFilledGoalInput === 2) {
-    goalInputList.addEventListener('input', () => {
-        progressBar.classList.remove('show-error')
+    goalInput.addEventListener('blur', () => {
+        if(goalInput.value) {
+            noOfFilledGoalInput++
+            console.log(noOfFilledGoalInput)
+        }
     })
-}
+    goalInput.addEventListener('input', () => {
+        if(noOfFilledGoalInput === 2) {
+            progressBar.classList.remove('show-error')
+        }
+    })
+})
